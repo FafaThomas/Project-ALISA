085b2448db7b4f0a1e78753fa1cd32bfe753cf01
FafaThomas <tdeclarino@gmail.com>
2026-09-14 11:13:22 +0800

    testing changes 2

--- services/codebase_ingestion_service/main.py
+++ services/codebase_ingestion_service/main.py
@@ -1,6 +1,7 @@
 from service import CodebaseIngestionService
 
 # GitLab migration test
+# Gitlab migration test 2
 PROJECT = r"C:\Users\tdecl\Downloads\ICTI_Website-main\ICTI_Website-main"
 
 ingestion = CodebaseIngestionService()
