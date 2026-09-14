c6d91ff1a678bdbeaa9897af3fed32d46c1004ba
FafaThomas <tdeclarino@gmail.com>
2026-09-14 11:29:31 +0800

    Removed the testing comments

--- services/codebase_ingestion_service/main.py
+++ services/codebase_ingestion_service/main.py
@@ -1,7 +1,6 @@
 from service import CodebaseIngestionService
 
-# GitLab migration test
-# Gitlab migration test 2
+
 PROJECT = r"C:\Users\tdecl\Downloads\ICTI_Website-main\ICTI_Website-main"
 
 ingestion = CodebaseIngestionService()
